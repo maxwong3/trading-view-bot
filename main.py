ee260bde90c2ef18d4ca716773e0a06ed6865ac1
maxwong3 <maxwon7@gmail.com>
2025-06-24 17:35:44 -0400

    fixed render port issue

--- main.py
+++ main.py
@@ -31,9 +31,11 @@ def webhook():
     else:
         abort(400)
 
+PORT = os.getenv('PORT')
+
 def run_flask():
     if __name__ == "__main__":
-        app.run(debug=True, host="0.0.0.0", port=80)
+        app.run(debug=True, host="0.0.0.0", port=PORT)
 
 
 # json setup

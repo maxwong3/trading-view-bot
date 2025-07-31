bcaa461837d0aac8ab70df407a23f6d19dbfb2e6
maxwong3 <maxwon7@gmail.com>
2025-07-31 18:18:32 -0400

    Update send_request.py

--- crypto-bot/send_request.py
+++ crypto-bot/send_request.py
@@ -7,11 +7,10 @@ url = "http://localhost/webhook"
 headers = {"Content-Type": "application/json"}
 
 payload = {
-    "ticker": "ETHUSD",
+    "ticker": "BTCUSD",
     "alert": "Moving up by 1% last hour",
-    "server_id": 1386798662616748153,  # Replace with your server ID
+    "server_id": 1400569573278875798,  # Replace with your server ID
     "time": "2025-07-28T15:34:00Z",
-    "signal_type":"buy",
     "secret": "secret",
     "open": 29500,
     "close": 29600,
